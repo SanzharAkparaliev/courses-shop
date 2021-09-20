0912bd7f7e062e9bdcd61045352886ef14fd55d4
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-09-20 11:42:08 +0600

    connect to MongoDb

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const mongoose =require('mongoose')
 
 const app = express()
 
@@ -25,8 +26,29 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
+
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+
+async function start(){
+  try {
+     const Mongo_url = 'mongodb+srv://sanzhar:FAebGccxUqWZTPC2@cluster0.set5o.mongodb.net/App?retryWrites=true&w=majority'
+
+      mongoose.connect(Mongo_url,{useNewUrlParser:true})
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+      })
+
+  } catch (er) {
+    console.log(err)
+  }
+
+
+
+}
+
+start( )
+
+
+
+
