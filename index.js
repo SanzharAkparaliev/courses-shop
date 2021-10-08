0c2fe22330cdf1ab255d299c9ec2a707a39b5a81
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-08 22:27:38 +0600

    added func remove from cart

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const mongoose =require('mongoose')
+const User = require('./models/user')
 
 const app = express()
 
@@ -18,6 +19,18 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req,res,next) => {
+  try {
+     const user =  await User.findById('61605d94300a655911c42f7c')
+       req.user = await  user
+       next()
+  } catch (error) {
+  console.log(error)  
+  }
+ 
+  
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
@@ -27,7 +40,7 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 // database connection
 async function start(){
@@ -35,11 +48,20 @@ async function start(){
      const Mongo_url = 'mongodb://localhost/nodeshop'
 
       mongoose.connect(Mongo_url)
+      const candidate =  await User.findOne()
+      if(!candidate){
+        const user = new User({
+          email:"sanzhar@gmail.com",
+          name:"sanzhar",
+          cart:{items:[]}
+        })
+        await user.save()
+      }
       app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
       })
 
-  } catch (er) {
+  } catch (err) {
     console.log(err)
   }
 
--- models/course.js
+++ models/course.js
@@ -1,15 +1,28 @@
-const {Schema,model} = require('mongoose')
+const {Schema, model} = require('mongoose')
 
-const course  = new Schema({
-  title:{
-    type:String,
-    required:true
+const courseSchema = new Schema({
+  title: {
+    type: String,
+    required: true
   },
-  price:{
-    type:Number,
-    require:true
+  price: {
+    type: Number,
+    required: true
   },
-  img:String
+  img: String,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
-module.exports = model('Course',course)
+courseSchema.method('toClient',function(){
+  const course = this.toObject()
+
+  course.id = course._id
+  delete course._id
+
+  return course
+})
+
+module.exports = model('Course',courseSchema)
