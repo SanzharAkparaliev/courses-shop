3c0b7fbe83b68a7652e16dfa97fb2797d95fd833
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-09 12:00:38 +0600

    withdrawal of orders

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const ordersRoutes = require('./routes/orders')
 const mongoose =require('mongoose')
 const User = require('./models/user')
 
@@ -38,9 +39,10 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/orders',ordersRoutes)
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 // database connection
 async function start(){
