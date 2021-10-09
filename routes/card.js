2eb12691956d45c86d24d4e9db9a02be54a2b8e8
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-09 16:26:23 +0600

    permission routes

--- routes/card.js
+++ routes/card.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
+const auth = require('../middleware/auth')
 
 function mapCartItems(cart){
  return cart.items.map(c => ({
@@ -15,13 +16,13 @@ function computePrice(courses){
   },0)
 }
 
-router.post('/add', async (req, res) => {
+router.post('/add',auth, async (req, res) => {
   const course = await Course.findById(req.body.id)
   await req.user.addToCart(course)
   res.redirect('/card')
 })
 
-router.delete('/remove/:id', async (req, res) => {
+router.delete('/remove/:id',auth, async (req, res) => {
   await req.user.removeFromCart(req.params.id)
   const user = await req.user.populate('cart.items.courseId')
 
@@ -33,7 +34,7 @@ router.delete('/remove/:id', async (req, res) => {
   res.status(200).json(cart)
 })
 
-router.get('/', async (req, res) => {
+router.get('/',auth, async (req, res) => {
   const user = await req.user 
     .populate('cart.items.courseId')
 
--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
+const auth = require('../middleware/auth')
+
 
 router.get('/', async (req, res) => {
   const courses = await Course.find()
@@ -11,7 +13,7 @@ router.get('/', async (req, res) => {
   })
 })
 
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit',auth, async (req, res) => {
   if (!req.query.allow) {
     return res.redirect('/')
   }
@@ -24,14 +26,14 @@ router.get('/:id/edit', async (req, res) => {
   })
 })
 
-router.post('/edit', async (req, res) => {
+router.post('/edit',auth, async (req, res) => {
   const {id} = req.body
   delete req.body.id
   await Course.findByIdAndUpdate(id,req.body)
   res.redirect('/courses')
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id',auth, async (req, res) => {
   const course = await Course.findById(req.params.id)
   res.render('course', {
     layout: 'empty',
@@ -40,7 +42,7 @@ router.get('/:id', async (req, res) => {
   })
 })
 
-router.post('/remove',async(req,res) => {
+router.post('/remove',auth, async(req,res) => {
   try {
     await Course.deleteOne({_id:req.body.id})
     res.redirect('/courses')
