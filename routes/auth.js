c0c41783c938a95cd42d4e7928e1a2b3131d55db
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-10-12 15:42:09 +0600

    async validation

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,14 @@ const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
 const nodemailer = require('nodemailer')
-const {body,validationResult} = require('express-validator')
+const {validationResult} = require('express-validator')
 const sendgrid = require('nodemailer-sendgrid-transport')
 const User = require('../models/user')
 const keys = require('../keys')
 const regEmail = require('../emails/registration')
 const resetEmail = require('../emails/reset')
 const router = Router()
+const {registerValidators} = require('../utils/validators')
 
 const transporter = nodemailer.createTransport(sendgrid({
   auth: {api_key: keys.SENDGRID_API_KEY}
@@ -59,28 +60,22 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/register',body('email').isEmail(),async (req, res) => {
+router.post('/register',registerValidators,async (req, res) => {
   try {
-    const {email, password, confirm, name} = req.body
-    const candidate = await User.findOne({ email })
+    const {email, password, name} = req.body
     const errors = validationResult(req)
     if(!errors.isEmpty()){
       req.flash('registerError',errors.array()[0].msg)
       return res.status(422).redirect('/auth/login#register')
     }
-
-    if (candidate) {
-      req.flash('registerError', 'Пользователь с таким email уже существует')
-      res.redirect('/auth/login#register')
-    } else {
       const hashPassword = await bcrypt.hash(password, 10)
       const user = new User({
         email, name, password: hashPassword, cart: {items: []}
-      })
+        })
       await user.save()
       await transporter.sendMail(regEmail(email))
       res.redirect('/auth/login#login')
-    }
+    
   } catch (e) {
     console.log(e)
   }
---
+++ utils/validators.js
@@ -0,0 +1,22 @@
+const {body} = require('express-validator')
+const User = require('../models/user')
+exports.registerValidators = [
+    body('email').isEmail().withMessage('Введите корректный email').custom(async(value,{req}) =>{
+        try {
+            const user =  await User.findOne({email:req.body.email})
+            if(user){
+                return Promise.reject('Такого email уже занят')
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }),
+    body('password','Пароль должен быть 6 символов').isLength({min:6,max:56}).isAlphanumeric(),
+    body('confirm').custom((value,{req}) => {
+        if(value !== req.body.password){
+            throw new Error('Пароли должны совпадать')
+        }
+        return true;
+    }),
+    body('name').isLength({min:3}).withMessage('Имя должно быть минимум 3 символа')
+]
